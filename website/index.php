14b15bbd7fc418e3ebaf1a5fe93743609a615e05
Dan Stowell <danstowell@users.sourceforge.net>
2008-09-14 14:02:16 +0000

    tweaks to website
    git-svn-id: https://sc3-plugins.svn.sourceforge.net/svnroot/sc3-plugins@261 a81c72af-a122-0410-9629-e4559562533e

--- website/index.php
+++ website/index.php
@@ -4,7 +4,7 @@
 body { background-color: rgb(204, 204, 204); font-family: Arial,Helvetica,sans-serif; }
 h1 {font-family: Arial,Helvetica,sans-serif; font-weight: normal;}
 
-.readonlinelink {font-size: smaller;}
+.readonlinelink {font-size: smaller; color: rgb(102,102,102); }
 
 li              { list-style-type: none; clear: both;}
 ul.grouplist    { width: 90%; clear: both; list-style-type: none; }
@@ -18,7 +18,7 @@ ul.grouplist li { width: 25%; float: left;  clear: none;}
 (see also <a href="http://quarks.sourceforge.net/">quarks</a> to extend the 
 programming language). For community discussion and support see the <a href="http://supercollider.sourceforge.net/mod/resource/view.php?id=41">SuperCollider mailing lists</a>.</p>
 
-<p style="padding: 10px; text-align: center; border: 1px solid blue; width: 50%;"><a href="http://sourceforge.net/project/platformdownload.php?group_id=182627">Download the plugins</a></p>
+<p style="padding: 10px; text-align: center; border: 1px solid blue; width: 50%; margin-left: 20%; "><a href="http://sourceforge.net/project/platformdownload.php?group_id=182627">Download the plugins</a></p>
 
 <p>The following list of helpfiles gives you an idea of the plugins you'll get
 when you download the plugin pack:</p>
@@ -48,7 +48,7 @@ foreach($grouped as $group=>$grlist){
 			<li><?php
 				echo $item['name'];
 				if(isset($item['fpath']))
-					echo " [<a class='readonlinelink' href='http://sc3-plugins.svn.sourceforge.net/viewvc/sc3-plugins/build/$item[fpath]'>read online</a>]";
+					echo " <span class='readonlinelink'>[<a href='http://sc3-plugins.svn.sourceforge.net/viewvc/sc3-plugins/build/$item[fpath]'>read online</a>]</span>";
 			?></li>
 			<?php
 		}
@@ -62,5 +62,5 @@ foreach($grouped as $group=>$grlist){
 </ul>
 
 <hr style="clear: both; margin: 10px; border-color: black;" />
-<p style="text-align: right;"><a href="http://sourceforge.net/projects/sc3-plugins/">Sourceforge project page</a><a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=182627&amp;type=4" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a></p>
+<p style="text-align: right;"><a href="http://sourceforge.net/projects/sc3-plugins/">Sourceforge project page</a> <a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=182627&amp;type=4" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a></p>
 </body></html>
