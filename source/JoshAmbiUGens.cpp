342332c0dcd9e703f5e48870833d4737aea5c371
Joshua Parmenter <joshpar@users.sourceforge.net>
2008-09-28 20:59:04 +0000

    fix r coefs in FMH encode and decode
    git-svn-id: https://sc3-plugins.svn.sourceforge.net/svnroot/sc3-plugins@265 a81c72af-a122-0410-9629-e4559562533e

--- source/JoshAmbiUGens.cpp
+++ source/JoshAmbiUGens.cpp
@@ -1449,7 +1449,7 @@ void BFEncodeSter_next(BFEncodeSter *unit, int inNumSamples)
     unit->m_X_amp = cosa * cosb * levelsinint; \
     unit->m_Y_amp = sina * cosb * levelsinint; \
     unit->m_Z_amp = sinb * levelsinint; \
-    unit->m_R_amp = (1.5 * sinbsq - 0.5) * levelsinint; \
+    unit->m_R_amp = (1.5 * sinbsq * -0.5) * levelsinint; \
     unit->m_S_amp = cosa * sinelevationtimes2 * levelsinint * twodivsqrt3; \
     unit->m_T_amp = sina * sinelevationtimes2 * levelsinint * twodivsqrt3; \
     unit->m_U_amp = cos(azimuthtimes2) * cosbsq * levelsinint * twodivsqrt3; \
@@ -1460,7 +1460,7 @@ void BFEncodeSter_next(BFEncodeSter *unit, int inNumSamples)
     unit->m_X_amp = cosa * cosb * level; \
     unit->m_Y_amp = sina * cosb * level; \
     unit->m_Z_amp = sinb * level; \
-    unit->m_R_amp = (1.5 * sinbsq - 0.5) * level; \
+    unit->m_R_amp = (1.5 * sinbsq * -0.5) * level; \
     unit->m_S_amp = cosa * sinelevationtimes2 * level * twodivsqrt3; \
     unit->m_T_amp = sina * sinelevationtimes2 * level * twodivsqrt3; \
     unit->m_U_amp = cos(azimuthtimes2) * cosbsq * level * twodivsqrt3; \
@@ -2005,7 +2005,7 @@ void BFDecode1_next(BFDecode1 *unit, int inNumSamples)
     unit->m_X_amp = cosa * cosb; \
     unit->m_Y_amp = sina * cosb; \
     unit->m_Z_amp = sinb; \
-    unit->m_R_amp = 1.5 * sinbsq - 0.5; \
+    unit->m_R_amp = 1.5 * sinbsq * -0.5; \
     unit->m_S_amp = cosa * sinelevationtimes2; \
     unit->m_T_amp = sina * sinelevationtimes2; \
     unit->m_U_amp = cos(azimuthtimes2) * cosbsq; \
