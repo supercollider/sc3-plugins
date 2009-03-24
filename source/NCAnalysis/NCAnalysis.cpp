3c1d4b317dc49689aec6bc33841ec7f4eb856507
Nick Collins <sicklincoln@users.sourceforge.net>
2009-03-24 22:50:10 +0000

    Adding new experimental analysis UGens
    git-svn-id: https://sc3-plugins.svn.sourceforge.net/svnroot/sc3-plugins@341 a81c72af-a122-0410-9629-e4559562533e

---
+++ source/NCAnalysis/NCAnalysis.cpp
@@ -0,0 +1,74 @@
+/*
+ *  NCAnalysis.cpp
+ *  xSC3ExtPlugins-Universal
+ *
+ *  Created by Nick Collins on 24/03/2009.
+ *  Copyright 2009 Nick Collins. All rights reserved.
+ *
+ */
+
+#include "NCAnalysis.h"
+
+
+
+extern "C"
+{
+	void load(InterfaceTable *inTable);
+}
+
+
+InterfaceTable *ft; 
+
+
+SCPolarBuf* ToPolarApx(SndBuf *buf);
+SCPolarBuf* ToPolarApx(SndBuf *buf)
+{
+	if (buf->coord == coord_Complex) {
+		SCComplexBuf* p = (SCComplexBuf*)buf->data;
+		int numbins = buf->samples - 2 >> 1;
+		for (int i=0; i<numbins; ++i) {
+			p->bin[i].ToPolarApxInPlace();
+		}
+		buf->coord = coord_Polar;
+	}
+	return (SCPolarBuf*)buf->data;
+}
+
+SCComplexBuf* ToComplexApx(SndBuf *buf);
+SCComplexBuf* ToComplexApx(SndBuf *buf)
+{
+	if (buf->coord == coord_Polar) {
+		SCPolarBuf* p = (SCPolarBuf*)buf->data;
+		int numbins = buf->samples - 2 >> 1;
+		for (int i=0; i<numbins; ++i) {
+			p->bin[i].ToComplexApxInPlace();
+		}
+		buf->coord = coord_Complex;
+	}
+	return (SCComplexBuf*)buf->data;
+}
+
+
+void init_SCComplex(InterfaceTable *inTable);
+
+extern void loadSMS(InterfaceTable *inTable);
+extern void loadTPV(InterfaceTable *inTable);
+
+void load(InterfaceTable *inTable)
+{
+	
+	ft= inTable;
+	
+	init_SCComplex(inTable);
+	
+	scfft_global_init();
+	
+	loadSMS(inTable); 
+	
+	loadTPV(inTable);
+	
+}
+
+
+
+
---
+++ source/NCAnalysis/NCAnalysis.h
@@ -0,0 +1,40 @@
+/*
+ *  NCAnalysis.h
+ *  xSC3ExtPlugins-Universal
+ *
+ *  Created by Nick Collins on 24/03/2009.
+ *  Copyright 2009 Nick Collins. All rights reserved.
+ *
+ */
+
+//#include <sndfile.h>
+#include "SC_PlugIn.h"
+#include "SC_Constants.h"
+
+//#include <vecLib/vecLib.h>
+#include <string.h>
+#include <math.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+
+
+#include "FFT_UGens.h"
+//#include "SC_fftlib.h"
+
+//
+//struct SCComplexBuf 
+//{
+//	float dc, nyq;
+//	SCComplex bin[1];
+//};
+//
+//struct SCPolarBuf 
+//{
+//	float dc, nyq;
+//	SCPolar bin[1];
+//};
+//
+//SCPolarBuf* ToPolarApx(SndBuf *buf);
+//SCComplexBuf* ToComplexApx(SndBuf *buf);
+
+extern InterfaceTable *ft;
