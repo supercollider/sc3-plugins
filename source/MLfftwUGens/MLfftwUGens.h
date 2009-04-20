7be28d205e58905cf0e70b6fbd48815d858ee133
Dan Stowell <danstowell@users.sourceforge.net>
2009-04-20 08:40:17 +0000

    Update address of Free Software Foundation (debian source-checking tools produce warnings about this)
    git-svn-id: https://sc3-plugins.svn.sourceforge.net/svnroot/sc3-plugins@359 a81c72af-a122-0410-9629-e4559562533e

--- source/MLfftwUGens/MLfftwUGens.h
+++ source/MLfftwUGens/MLfftwUGens.h
@@ -15,7 +15,7 @@
  
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
 //This file is part of MachineListening. Copyright (C) 2006  Nicholas M.Collins distributed under the terms of the GNU General Public License full notice in file MachineListening.license
--- source/NCAnalysis/TPV.cpp
+++ source/NCAnalysis/TPV.cpp
@@ -15,7 +15,7 @@
  
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
 //Tracking Phase Vocoder following McAualay and Quatieri model from IEEE Trans acoustics, speech and signal processing vol assp-34(4) aug 1986
