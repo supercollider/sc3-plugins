fc9b29c6b87d739a0a45a1d98011e8640dfbdf5e
Tim Blechmann <tim@klingt.org>
2012-01-23 13:30:23 +0100

    JoshUGens: MoogVCF - use fast exp approximation
    instead of calling the libm implementation of exp, we use a faster, but
    less accurate approximation.
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- source/JoshUGens/JoshUGens.cpp
+++ source/JoshUGens/JoshUGens.cpp
@@ -2477,11 +2477,72 @@ MoogVCF.ar(in, fco, res, mul, add); fco = filter cut-off, res = resonance (0 -> 
 
 */
 
+// fastexp taken from:
+
+/*=====================================================================*
+ *                   Copyright (C) 2011 Paul Mineiro                   *
+ * All rights reserved.                                                *
+ *                                                                     *
+ * Redistribution and use in source and binary forms, with             *
+ * or without modification, are permitted provided that the            *
+ * following conditions are met:                                       *
+ *                                                                     *
+ *     * Redistributions of source code must retain the                *
+ *     above copyright notice, this list of conditions and             *
+ *     the following disclaimer.                                       *
+ *                                                                     *
+ *     * Redistributions in binary form must reproduce the             *
+ *     above copyright notice, this list of conditions and             *
+ *     the following disclaimer in the documentation and/or            *
+ *     other materials provided with the distribution.                 *
+ *                                                                     *
+ *     * Neither the name of Paul Mineiro nor the names                *
+ *     of other contributors may be used to endorse or promote         *
+ *     products derived from this software without specific            *
+ *     prior written permission.                                       *
+ *                                                                     *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND              *
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,         *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES               *
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE             *
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER               *
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                 *
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES            *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE           *
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR                *
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF          *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY              *
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE             *
+ * POSSIBILITY OF SUCH DAMAGE.                                         *
+ *                                                                     *
+ * Contact: Paul Mineiro <paul@mineiro.com>                            *
+ *=====================================================================*/
+
+static inline float
+fastpow2 (float p)
+{
+	float offset = (p < 0) ? 1.0f : 0.0f;
+	float clipp = (p < -126) ? -126.0f : p;
+	int w = clipp;
+	float z = clipp - w + offset;
+	union { uint32_t i; float f; } v = { (1 << 23) * (clipp + 121.2740575f + 27.7280233f / (4.84252568f - z) - 1.49012907f * z) };
+
+	return v.f;
+}
+
+static inline float
+fastexp (float p)
+{
+	return fastpow2 (1.442695040f * p);
+}
+
+
 static inline void MoogVCF_calc_parameters(float fco, float res, float & kp, float & pp1d2, float & k)
 {
 	kp = (3.6f * fco) - ((1.6f * fco) * fco) - 1.0f;  /* Emperical tuning     */
 	pp1d2 = (kp + 1.0f) * 0.5f;              /* Timesaver                  */
-	float scale = exp((double)((1.0f - pp1d2 ) * 1.386249f)); /* Scaling factor  */
+	float scale = fastexp((1.0f - pp1d2 ) * 1.386249f); /* Scaling factor  */
 	k = res * scale;
 }
 
