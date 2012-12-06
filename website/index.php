8723ccd904f11f577e30a7ad3b67263055f2079f
Dan Stowell <danstowell@users.sourceforge.net>
2012-12-06 09:09:17 +0000

    Simplify sf.net webpage - point towards github source

--- website/index.php
+++ website/index.php
@@ -18,48 +18,12 @@ ul.grouplist li { width: 25%; float: left;  clear: none;}
 (see also <a href="http://quarks.sourceforge.net/">quarks</a> to extend the 
 programming language). For community discussion and support see the <a href="http://supercollider.sourceforge.net/mod/resource/view.php?id=41">SuperCollider mailing lists</a>.</p>
 
-<p style="padding: 10px; text-align: center; border: 1px solid blue; width: 50%; margin-left: 20%; "><a href="http://sourceforge.net/project/platformdownload.php?group_id=182627">Download the plugins</a></p>
 
-<p>The following list of helpfiles gives you an idea of the plugins you'll get
-when you download the plugin pack:</p>
+<p style="margin: 30px; padding: 30px; border: 1px solid black;">Downloads are available from <a href="https://sourceforge.net/projects/sc3-plugins/files/">https://sourceforge.net/projects/sc3-plugins/files/</a>
 
-<ul>
-<?php
-$baseUrl = 'http://sc3-plugins.git.sourceforge.net/git/gitweb.cgi?p=sc3-plugins/sc3-plugins;a=blob_plain;hb=refs/heads/master;f=source';
-$list = file('filelist.txt');
-foreach($list as $key =>$fpath){
-	$group = array_shift(explode('/', $fpath));
-	$filename = trim(basename($fpath));
-	$ext = substr($filename, strrpos($filename, '.'));
-	if($ext == '.html' && basename($filename, '.html') != $group) {
-		$grouped[$group][] = array('name'=>substr($filename, 0, -5), 'fpath'=>$fpath);
-	}else{
-		$grouped[$group][] = array('name'=>substr($filename, 0, strpos($filename, '.')));
-	}
-}
-
-foreach($grouped as $group=>$grlist){
-	?>
-	<li><strong><?php echo $group ?></strong>:</li>
-	<ul class="grouplist">
-		<?php
-		foreach($grlist as $item){
-			?>
-			<li><?php
-				echo $item['name'];
-				if(isset($item['fpath']))
-					echo " <span class='readonlinelink'>[<a href='$baseUrl/$item[fpath]'>read online</a>]</span>";
-			?></li>
-			<?php
-		}
-		?>
-	</ul>
-	<?php
-}
+<p style="margin: 30px; padding: 30px; border: 1px solid black;"><a href="https://github.com/supercollider/sc3-plugins">Source code is on github</a></p>
 
 
-?>
-</ul>
 
 <hr style="clear: both; margin: 10px; border-color: black;" />
 <p style="text-align: right;"><a href="http://sourceforge.net/projects/sc3-plugins/">Sourceforge project page</a> <a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=182627&amp;type=4" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a></p>
