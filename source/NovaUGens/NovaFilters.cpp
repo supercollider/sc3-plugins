a5789200f551919aa3ec1d40f9257aac6a4a5482
Tim Blechmann <tim@klingt.org>
2013-02-03 13:46:45 +0100

    nova ugens: DiodeLadderFilter - better templating for parameters
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- source/NovaUGens/NovaFilters.cpp
+++ source/NovaUGens/NovaFilters.cpp
@@ -89,6 +89,49 @@ struct QParameter<slope>
 	double kSlope_, ASlope_;
 };
 
+
+template <int ArgType>
+struct HPFParameter
+{};
+
+
+template <>
+struct HPFParameter<scalar>
+{
+	HPFParameter(double a, double b):
+		a_(a), b_(b)
+	{}
+
+	void getParameters(double & a, double & b)
+	{
+		a = a_;
+		b = b_;
+	}
+
+	double a_, b_;
+};
+
+template <>
+struct HPFParameter<slope>
+{
+	HPFParameter(double a, double aSlope, double b, double bSlope):
+		a_(a), b_(b), aSlope(aSlope), bSlope(bSlope)
+	{}
+
+	void getParameters(double & a, double & b)
+	{
+		a = a_;
+		b = b_;
+		a_ += aSlope;
+		b_ += bSlope;
+	}
+
+	double a_, b_;
+	double aSlope, bSlope;
+};
+
+
+
 // TODO: add some oversampling
 struct DiodeLadderFilter:
 	public SCUnit
@@ -97,88 +140,155 @@ public:
 	DiodeLadderFilter()
 	{
 		std::fill(z, z + 5, 0);
-		hpCutoff = std::numeric_limits<float>::signaling_NaN();
 
-		float newQ = in0(2);
+		float newQ         = in0(2);
+		float newHPCutoff  = in0(3);
 		set_q(newQ);
+
+		setFeedbackHPF(newHPCutoff * sampleDur());
+		hpCutoff = newHPCutoff;
 
 		if (inRate(1) == calc_FullRate) {
 			switch (inRate(2)) {
-			case calc_ScalarRate:
-				set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next_xi<true> >();
-				break;
+			case calc_ScalarRate: {
+				switch (inRate(3)) {
+				case calc_ScalarRate:
+					set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next<true, calc_ScalarRate, calc_ScalarRate> >();
+					return;
 
-			case calc_BufRate:
-			default:
-				set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next_xk<true> >();
-				break;
+				default:
+					set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next<true, calc_ScalarRate, calc_BufRate> >();
+					return;
+				}
+			}
+
+			default: {
+				switch (inRate(3)) {
+				case calc_ScalarRate:
+					set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next<true, calc_BufRate, calc_ScalarRate> >();
+					return;
+
+				default:
+					set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next<true, calc_BufRate, calc_BufRate> >();
+					return;
+				}
+			}
 			}
 		} else {
 			switch (inRate(2)) {
-			case calc_ScalarRate:
-				set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next_xi<false> >();
-				break;
+			case calc_ScalarRate: {
+				switch (inRate(3)) {
+				case calc_ScalarRate:
+					set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next<false, calc_ScalarRate, calc_ScalarRate> >();
+					return;
 
-			case calc_BufRate:
-			default:
-				set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next_xk<false> >();
-				break;
+				default:
+					set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next<false, calc_ScalarRate, calc_BufRate> >();
+					return;
+				}
+			}
+
+			default: {
+				switch (inRate(3)) {
+				case calc_ScalarRate:
+					set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next<false, calc_BufRate, calc_ScalarRate> >();
+					return;
+
+				default:
+					set_calc_function<DiodeLadderFilter, &DiodeLadderFilter::next<false, calc_BufRate, calc_BufRate> >();
+					return;
+				}
+			}
 			}
 		}
 	}
 
 private:
-	template <bool AudioRateFrequency, typename QParam>
-	void next(int inNumSamples, QParam & qParameter)
+	template <bool AudioRateFrequency, typename QParam, typename HPFParam>
+	void next_(int inNumSamples, QParam & qParameter, HPFParam & hpfParam)
 	{
 		if (AudioRateFrequency)
-			next_a(inNumSamples, qParameter);
+			next_a(inNumSamples, qParameter, hpfParam);
 		else
-			next_k(inNumSamples, qParameter);
+			next_k(inNumSamples, qParameter, hpfParam);
 	}
 
-	template <bool AudioRateFrequency>
-	void next_xi(int inNumSamples)
+	template <bool AudioRateFrequency, int QRate, int HPFRate>
+	void next(int inNumSamples)
 	{
-		QParameter<scalar> qParam(m_k, m_A);
-		next<AudioRateFrequency>(inNumSamples, qParam);
-	}
+		switch (QRate) {
+		case calc_ScalarRate: {
+			QParameter<scalar> qParam(m_k, m_A);
+			next_selectHPF<AudioRateFrequency, HPFRate>(inNumSamples, qParam);
+			return;
+		}
 
-	template <bool AudioRateFrequency>
-	void next_xk(int inNumSamples)
-	{
-		float newQ = in0(2);
-		newQ = sc_clip(newQ, 0, 1);
+		default: {
+			float newQ = in0(2);
+			newQ = sc_clip(newQ, 0, 1);
 
-		if (newQ != q) {
-			double oldA = m_A, oldk = m_k;
-			double newA, newk;
+			if (newQ != q) {
+				double oldA = m_A, oldk = m_k;
+				double newA, newk;
 
-			update_kA(newQ, newk, newA);
-			m_A = newA;
-			m_k = newk;
+				update_kA(newQ, newk, newA);
+				m_A = newA;
+				m_k = newk;
+				q = newQ;
 
-			double kSlope = calcSlope(newk, oldk);
-			double ASlope = calcSlope(newA, oldA);
+				double kSlope = calcSlope(newk, oldk);
+				double ASlope = calcSlope(newA, oldA);
 
-			QParameter<slope> qParam(oldk, kSlope, oldA, ASlope);
-			next<AudioRateFrequency>(inNumSamples, qParam);
-		} else {
-			next_xi<AudioRateFrequency>(inNumSamples);
+				QParameter<slope> qParam(oldk, kSlope, oldA, ASlope);
+				next_selectHPF<AudioRateFrequency, HPFRate>(inNumSamples, qParam);
+			} else {
+				QParameter<scalar> qParam(m_k, m_A);
+				next_selectHPF<AudioRateFrequency, HPFRate>(inNumSamples, qParam);
+			}
+			return;
+		}
 		}
 	}
 
-	template <typename QParam>
-	void next_k(int inNumSamples, QParam & qParameter)
+	template <bool AudioRateFrequency, int HPFRate, typename QParam>
+	void next_selectHPF(int inNumSamples, QParam & qParam)
 	{
-		float newFreq        = in0(1);
-		float newHPCutoff    = in0(3);
+		switch (HPFRate) {
+		case calc_ScalarRate: {
+			HPFParameter<scalar> hpfParam(m_ah, m_bh);
+			next_<AudioRateFrequency>(inNumSamples, qParam, hpfParam);
+			return;
+		}
 
-		if (newHPCutoff != hpCutoff)
-			setFeedbackHPF(newHPCutoff * sampleDur());
+		default: {
+			float newHPCutoff    = in0(3);
+			if (newHPCutoff != hpCutoff) {
+				double oldA = m_ah, oldB = m_bh;
+				double newA, newB;
+				double fc = newHPCutoff * sampleDur();
 
+				feedbackHPF(fc, newA, newB);
+				m_ah = newA;
+				m_bh = newB;
+				hpCutoff = newHPCutoff;
+
+				double slopeA = calcSlope(newA, oldA);
+				double slopeB = calcSlope(newB, oldB);
+
+				HPFParameter<slope> hpfParam(oldA, slopeA, oldB, slopeB);
+				next_<AudioRateFrequency>(inNumSamples, qParam, hpfParam);
+			} else {
+				HPFParameter<scalar> hpfParam(m_ah, m_bh);
+				next_<AudioRateFrequency>(inNumSamples, qParam, hpfParam);
+			}
+		}
+		}
+	}
+
+	template <typename QParam, typename HPFParam>
+	void next_k(int inNumSamples, QParam & qParameter, HPFParam & hpfParam)
+	{
 		double a, a_inv, a2, b, b2, c, g, g0;
-		calcFilterCoefficients(newFreq, a, a2, a_inv, b, b2, c, g, g0);
 
 		double z0 = z[0];
 		double z1 = z[1];
@@ -189,14 +299,30 @@ private:
 		const float * inSig = zin(0);
 		float * outSig = zout(0);
 
-		for (int i = 0; i != inNumSamples; ++i) {
-			double x = ZXP(inSig);
-			double A, k;
+		float newFreq        = in0(1);
 
-			qParameter.getParameters(k, A);
+		if (newFreq == freq) {
+			calcFilterCoefficients(freq, a, a2, a_inv, b, b2, c, g, g0);
 
-			ZXP(outSig) = tick(x, a, a2, a_inv, b, b2, c, g, g0, z0, z1, z2, z3, z4, k, A);
+			loop(inNumSamples, [&] {
+				double x = ZXP(inSig);
+
+				ZXP(outSig) = tick(x, a, a2, a_inv, b, b2, c, g, g0, z0, z1, z2, z3, z4, qParameter, hpfParam);
+			});
+		} else {
+			float oldfreq = freq;
+			float freqSlope = calcSlope(newFreq, oldfreq);
+			freq = newFreq;
+
+			loop(inNumSamples, [&] {
+				double x = ZXP(inSig);
+				calcFilterCoefficients(oldfreq, a, a2, a_inv, b, b2, c, g, g0);
+				oldfreq += freqSlope;
+
+				ZXP(outSig) = tick(x, a, a2, a_inv, b, b2, c, g, g0, z0, z1, z2, z3, z4, qParameter, hpfParam);
+			});
 		}
+
 		z[0] = z0;
 		z[1] = z1;
 		z[2] = z2;
@@ -204,14 +330,9 @@ private:
 		z[4] = z4;
 	}
 
-	template <typename QParam>
-	void next_a(int inNumSamples, QParam & qParameter)
+	template <typename QParam, typename HPFParam>
+	void next_a(int inNumSamples, QParam & qParameter, HPFParam & hpfParam)
 	{
-		float newHPCutoff    = in0(3);
-
-		if (newHPCutoff != hpCutoff)
-			setFeedbackHPF(newHPCutoff * sampleDur());
-
 		const float * inSig = zin(0);
 		const float * inFreq = zin(1);
 		float * outSig = zout(0);
@@ -227,11 +348,8 @@ private:
 			double a, a_inv, a2, b, b2, c, g, g0;
 			calcFilterCoefficients(freq, a, a2, a_inv, b, b2, c, g, g0);
 
-			double k, A;
-			qParameter.getParameters(k, A);
-
 			double x = ZXP(inSig);
-			ZXP(outSig) = tick(x, a, a2, a_inv, b, b2, c, g, g0, z0, z1, z2, z3, z4, k, A);
+			ZXP(outSig) = tick(x, a, a2, a_inv, b, b2, c, g, g0, z0, z1, z2, z3, z4, qParameter, hpfParam);
 		}
 		z[0] = z0;
 		z[1] = z1;
@@ -240,9 +358,16 @@ private:
 		z[4] = z4;
 	}
 
-	double tick(double x, double a, double a2, double ainv, double b, double b2, double c, double g, double g0,
-				double & z0, double & z1, double & z2, double & z3, double & z4, double k, double A)
+	template <typename QParam, typename HPFParam>
+	static double tick(double x, double a, double a2, double ainv, double b, double b2, double c, double g, double g0,
+					   double & z0, double & z1, double & z2, double & z3, double & z4, QParam & qParameter, HPFParam & hpfParam)
 	{
+		double k, A;
+		qParameter.getParameters(k, A);
+
+		double ah, bh;
+		hpfParam.getParameters(ah, bh);
+
 		// current state
 		const double s0 = (a2*a*z0 + a2*b*z1 + z2*(b2 - 2*a2)*a + z3*(b2 - 3*a2)*b) * c;
 		const double s = bh*s0 - z4;
@@ -287,12 +412,16 @@ private:
 		b2 = b*b;
 		c = 1 / (2*a2*a2 - 4*a2*b2 + b2*b2);
 		g0 = 2*a2*a2*c;
-		g = g0 * bh;
+		g = g0 * m_bh;
 	}
 
 	void setFeedbackHPF(double fc)
 	{
-		hpCutoff = fc;
+		feedbackHPF(fc, m_ah, m_bh);
+	}
+
+	static void feedbackHPF(double fc, double & ah, double & bh)
+	{
 		const double K = fc * M_PI;
 		ah = (K - 2) / (K + 2);
 		bh = 2 / (K + 2);
@@ -315,10 +444,10 @@ private:
 		return sample / (1+ std::abs(sample));
 	}
 
-	float q, hpCutoff;
+	float freq, q, hpCutoff;
 	double m_k, m_A;
 	double z[5];
-	double ah, bh;
+	double m_ah, m_bh;
 };
 
 DEFINE_XTORS(DiodeLadderFilter)
