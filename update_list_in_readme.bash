89f0917db0eec4cc3adf601bff4c99f9b7817b01
Yvan Volochine <yvan.volochine@gmail.com>
2013-04-13 16:45:42 +0200

    documentation: improve README and various cleanup
     - move install, dev and package infos into README
     - list plugins helpfiles into a separate plugins_list.md file
     - delete obsolete VERSION file (and INSTALL, readme_devs.txt and readme_package.txt)

--- update_list_in_readme.bash
+++ update_list_in_readme.bash
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-READMEPATH=README.md
-TEMPPATH=README.md.tmp
+FILE_PATH=plugins_list.md
 
-# take a copy of the readme, with any generated links filtered back out
-cat "$READMEPATH" | sed -Ee "/^\* \[\w+\]\(.*\)$/d" > "$TEMPPATH"
+echo "" > $FILE_PATH
 
 # find .schelp files, and use it to generate links to scdoc
-#for f in `find source/*/sc -name "*.schelp" | sort | sed s:..\/source\/::`
 for h in `find source -name "*.schelp" | sed -Ee "s/^.*\/(.*)\.schelp/\\1/g" -e '/-/d' | sort`
 do
-	echo "* [$h](http://doc.sccode.org/Classes/$h.html)" >> "$TEMPPATH"
+	echo "* [$h](http://doc.sccode.org/Classes/$h.html)" >> "$FILE_PATH"
 done
-
-# copy the finished readme back into place
-mv -f "$TEMPPATH" "$READMEPATH"
-
