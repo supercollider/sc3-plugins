3937453eaf0aa0f9861506d290597d521201e5d4
Tim Blechmann <tim@klingt.org>
2013-07-15 23:42:05 +0200

    Tartini: supernova compile fix
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- source/PitchDetection/Tartini.h
+++ source/PitchDetection/Tartini.h
@@ -21,7 +21,7 @@
 #define SC_FFT_FFTW 0
 #define SC_FFT_VDSP 0
 #define SC_FFT_GREEN 1
-#elif !SC_FFT_FFTW && defined(__APPLE__) && !defined(SUPERNOVA)
+#elif !SC_FFT_FFTW && defined(__APPLE__)
 #define SC_FFT_FFTW 0
 #define SC_FFT_VDSP 1
 #define SC_FFT_GREEN 0
