7b64e68f1ba35c4eb55a27a62dc479d0d2875c42
Victor Bombi <sonoro@telefonica.net>
2015-10-17 19:22:32 +0200

    OteyPiano: new Banks hammer model, use t_gate for retriggering instead of release, use DWGUGens dwglib.

--- source/OteyPianoUGens/hammer.cpp
+++ source/OteyPianoUGens/hammer.cpp
@@ -4,31 +4,51 @@
 
 Hammer :: Hammer(float f, float Fs, float m, float K, float p, float Z, float alpha, float v0) 
 {
-  this->S = 3;
+
   this->Fs = Fs;
   this->p = p;
   this->K = K;
   this->mi = 1.0/m;
-  this->alpha = alpha;
+
   this->Z2i = 1.0/(2.0*Z);
-  this->v0 = v0;
+
+  this->a = 0.0;
+  this->F = 0.0;
+
+}
+BanksHammer :: BanksHammer(float f, float Fs, float m, float K, float p, float Z, float alpha, float v0) :Hammer(f,Fs,m,K, p,Z,alpha,v0)
+{
+
+  intv.Init(Fs*2);
+  intvh.Init(Fs*2);
+  undel.Init();
+  vh = 0.0;
+  oldvin = 0.0;
+  intvh.go(v0*Fs*2);
+}
+StulovHammer :: StulovHammer(float f, float Fs, float m, float K, float p, float Z, float alpha, float v0):Hammer(f,Fs,m,K, p,Z,alpha,v0)
+{
+  this->S = 3;
+  this->alpha = alpha;
   this->dt = 1.0/(Fs*S);
   this->dti = 1.0/dt;
   this->x = 0;
   this->v = v0;
-  this->a = 0.0;
-  this->F = 0.0;
   this->upprev = 0;
+
 }
 
 Hammer :: ~Hammer() {
 }
-float Hammer::trigger(float v){
-   this->v0 = v;
+BanksHammer :: ~BanksHammer() {
+}
+void StulovHammer::trigger(float v){
+  // this->v0 = v;
    this->v = v;
    this->x = 0.0;
 }
-float Hammer :: load(float vin) {
+
+float StulovHammer :: load(float vin) {
   for(int k=0;k<S;k++) {
     float up;
     up = (x>0)?pow(x,p):0;
@@ -52,4 +72,39 @@ float Hammer :: load(float vin) {
   }
 
   return F;
+}
+void BanksHammer::trigger(float v){
+    this->F = 0.0;
+   this->vh = 0.0;
+   //intvh.lastout = 0.0;
+  intv.Init(Fs*2);
+  intvh.Init(Fs*2);
+  undel.Init();
+   intvh.go(v*Fs*2);
+}
+
+float BanksHammer::load(float vin){
+    float rvin = (vin + oldvin)*0.5;
+    float F1,F2;
+    
+    float vs = (rvin +F*Z2i);
+    float deltaV = vh - vs;
+    float deltaY = intv.go(deltaV);
+    float up = (deltaY>0.0)?pow(deltaY,p):0.0;
+    up = up*K;
+    a = -F*mi;
+    vh = intvh.go(a);
+    F1 = F = undel.go(up);
+    
+    vs = (vin +F*Z2i);
+    deltaV = vh - vs;
+    deltaY = intv.go(deltaV);
+    up = (deltaY>0.0)?pow(deltaY,p):0.0;
+    up = up*K;
+    a = -F*mi;
+    vh = intvh.go(a);
+    F2 = F = undel.go(up);
+    
+    
+    return (F1 + F2)*0.5; 
 }
--- source/OteyPianoUGens/piano.h
+++ source/OteyPianoUGens/piano.h
@@ -22,7 +22,7 @@ class Piano {
   Piano(Unit * unit=0);
   ~Piano();
 
- void init(float f, float Fs, float velocity, float minr,float maxr,float ampr,float centerr, float mult_radius_core_string, float minL,float maxL,float ampL,float centerL, float mult_density_string, float mult_modulus_string, float mult_impedance_bridge, float mult_impedance_hammer, float mult_mass_hammer, float mult_force_hammer, float mult_hysteresis_hammer, float mult_stiffness_exponent_hammer, float position_hammer, float mult_loss_filter,float detune);
+ void init(float f, float Fs, float velocity, float minr,float maxr,float ampr,float centerr, float mult_radius_core_string, float minL,float maxL,float ampL,float centerL, float mult_density_string, float mult_modulus_string, float mult_impedance_bridge, float mult_impedance_hammer, float mult_mass_hammer, float mult_force_hammer, float mult_hysteresis_hammer, float mult_stiffness_exponent_hammer, float position_hammer, float mult_loss_filter,float detune,int hammer_type=1);
 
   long go(float *out, int samples);
   float getResampleRatio();
