ba95361060956a9f61e76c7229d8b2685db6d4ac
Victor Bombi <sonoro@telefonica.net>
2015-11-29 16:17:57 +0100

    StkInst: some indentations

--- source/StkInst/StkInst.cpp
+++ source/StkInst/StkInst.cpp
@@ -141,86 +141,86 @@ int voiceByNumber(int number, Instrmnt **instrument)
 
 struct StkInst : public Unit
 {
-	Instrmnt * inst;
-	bool  gate;
-	float freq;
-	float ampat;
-	float amprel;
+    Instrmnt * inst;
+    bool  gate;
+    float freq;
+    float ampat;
+    float amprel;
 
-	int m_valueSize;
-	int m_valueOffset;
-	float *m_values;
-	float *old_values;
-	bool do_freq;
-	int instNumber;
-	//StkInst(Unit *unit);
-	//~StkInst(){};
+    int m_valueSize;
+    int m_valueOffset;
+    float *m_values;
+    float *old_values;
+    bool do_freq;
+    int instNumber;
+    //StkInst(Unit *unit);
+    //~StkInst(){};
 };
 
 extern "C"
 {
-	void StkInst_next(StkInst *unit, int inNumSamples);
-	void StkInst_Ctor(StkInst* unit);
-	void StkInst_Dtor(StkInst* unit);
+    void StkInst_next(StkInst *unit, int inNumSamples);
+    void StkInst_Ctor(StkInst* unit);
+    void StkInst_Dtor(StkInst* unit);
 }
 
 static void Unit_next_nop(StkInst * unit, int inNumSamples)
 {}
 void StkInst_Ctor(StkInst* unit) {
 
-	gWorld = unit->mWorld;
-	Stk::setSampleRate( SAMPLERATE );
+    gWorld = unit->mWorld;
+    Stk::setSampleRate( SAMPLERATE );
 
-	unit->gate = false;
-	unit->freq = IN0(0);
-	unit->ampat = 1;
-	unit->amprel = 1;
-	unit->m_values = 0;
-	unit->old_values = 0;
-	unit->do_freq = true;
-	const int kVarOffset = 5;
+    unit->gate = false;
+    unit->freq = IN0(0);
+    unit->ampat = 1;
+    unit->amprel = 1;
+    unit->m_values = 0;
+    unit->old_values = 0;
+    unit->do_freq = true;
+    const int kVarOffset = 5;
 
-	unit->instNumber = IN0(4);
-	unit->m_valueSize = unit->mNumInputs - kVarOffset;
-	//Print("StkInst\n");
-	//Print("StkInst %d,%f;%f,%d,%d\n",unit->instNumber,unit->freq,unit->ampat,unit->mNumInputs,unit->m_valueSize);
-	//make it pair
-	unit->m_valueSize = floor(unit->m_valueSize /2.0) * 2.0;
-	if(unit->m_valueSize > 0){
-		const int valuesAllocSize = unit->m_valueSize * sizeof(float);
-		char * chunk = (char*)RTAlloc(unit->mWorld, valuesAllocSize*2);
-		if (!chunk) {
-			Print("StkInst: RT memory allocation failed on values\n");
-			SETCALC(Unit_next_nop);
-			return;
-		}
-		unit->m_values  = (float*)chunk;
-		unit->old_values = (float*)(chunk + valuesAllocSize);
-		for (int i=0; i<unit->m_valueSize; i++)
-			unit->old_values[i] = -1; //set old values to something imposible
-	}
+    unit->instNumber = IN0(4);
+    unit->m_valueSize = unit->mNumInputs - kVarOffset;
+    //Print("StkInst\n");
+    //Print("StkInst %d,%f;%f,%d,%d\n",unit->instNumber,unit->freq,unit->ampat,unit->mNumInputs,unit->m_valueSize);
+    //make it pair
+    unit->m_valueSize = floor(unit->m_valueSize /2.0) * 2.0;
+    if(unit->m_valueSize > 0){
+        const int valuesAllocSize = unit->m_valueSize * sizeof(float);
+        char * chunk = (char*)RTAlloc(unit->mWorld, valuesAllocSize*2);
+        if (!chunk) {
+            Print("StkInst: RT memory allocation failed on values\n");
+            SETCALC(Unit_next_nop);
+            return;
+        }
+        unit->m_values  = (float*)chunk;
+        unit->old_values = (float*)(chunk + valuesAllocSize);
+        for (int i=0; i<unit->m_valueSize; i++)
+            unit->old_values[i] = -1; //set old values to something imposible
+    }
 
-	unit->inst = NULL;
-	try {
-		if(voiceByNumber(unit->instNumber,&(unit->inst))==-1){
-			Print("StkInst: RT memory allocation failed on instrument\n");
-			SETCALC(Unit_next_nop);
-			return;
-		}
-	}
-	catch ( StkError & e) {
-		Print("StkInst: Error on creation %s",e.getMessageCString());
-		SETCALC(Unit_next_nop);
-		return;
-	}
+    unit->inst = NULL;
+    try {
+        if(voiceByNumber(unit->instNumber,&(unit->inst))==-1){
+            Print("StkInst: RT memory allocation failed on instrument\n");
+            SETCALC(Unit_next_nop);
+            return;
+        }
+    }
+    catch ( StkError & e) {
+        Print("StkInst: Error on creation %s",e.getMessageCString());
+        SETCALC(Unit_next_nop);
+        return;
+    }
 
-	SETCALC(StkInst_next);
+    SETCALC(StkInst_next);
 }
 
 void StkInst_Dtor(StkInst* unit)
 {
-	//delete unit->m_values;
-	//delete unit->inst;
+    //delete unit->m_values;
+    //delete unit->inst;
     RTFree(unit->mWorld, unit->m_values);
     RTFree(unit->mWorld, unit->inst);
 
@@ -228,194 +228,194 @@ void StkInst_Dtor(StkInst* unit)
 
 void StkInst_next(StkInst *unit, int inNumSamples)
 {
-	float *out = OUT(0);
+    float *out = OUT(0);
 
-	float freq  	= IN0(0);
-	bool  gate		= IN0(1) > 0.f;
-	float  ampat		= IN0(2);
-	float  amprel		= IN0(3);
+    float freq      = IN0(0);
+    bool  gate      = IN0(1) > 0.f;
+    float  ampat        = IN0(2);
+    float  amprel       = IN0(3);
 
-	
-	float *values = unit->m_values;
-	float *old_values = unit->old_values;
-	int valueSize = unit->m_valueSize;
-	int valueOffset = 5;
-	for (int i=0; i<valueSize; i++)
-		values[i] = IN0(i + valueOffset);
+    
+    float *values = unit->m_values;
+    float *old_values = unit->old_values;
+    int valueSize = unit->m_valueSize;
+    int valueOffset = 5;
+    for (int i=0; i<valueSize; i++)
+        values[i] = IN0(i + valueOffset);
 
-	for(int i=0; i < valueSize ; i+=2){
-		if(values[i+1] != old_values[i + 1]){
-			unit->inst->controlChange(values[i], values[i+1]);
-			old_values[i + 1] = values[i+1];
-		}
-	}
-	if(unit->do_freq && freq != unit->freq){
-		unit->inst->setFrequency(freq);
-		unit->freq = freq;
-	}
-	
-	if(gate != unit->gate){
-		if(gate){
-			//Print("Starting freq %f\n",freq);
-		    unit->inst->noteOn(freq, ampat);
-		}else{
-			//Print("Stopping\n");
-			unit->inst->noteOff(amprel);
-		}
-		unit->gate = gate;
-	}
+    for(int i=0; i < valueSize ; i+=2){
+        if(values[i+1] != old_values[i + 1]){
+            unit->inst->controlChange(values[i], values[i+1]);
+            old_values[i + 1] = values[i+1];
+        }
+    }
+    if(unit->do_freq && freq != unit->freq){
+        unit->inst->setFrequency(freq);
+        unit->freq = freq;
+    }
+    
+    if(gate != unit->gate){
+        if(gate){
+            //Print("Starting freq %f\n",freq);
+            unit->inst->noteOn(freq, ampat);
+        }else{
+            //Print("Stopping\n");
+            unit->inst->noteOff(amprel);
+        }
+        unit->gate = gate;
+    }
 
- 	for (int i=0; i < inNumSamples; ++i)
-	{
-		out[i] = unit->inst->tick();// * 7.5f; // Scaled to approx +-1
-	}
+    for (int i=0; i < inNumSamples; ++i)
+    {
+        out[i] = unit->inst->tick();// * 7.5f; // Scaled to approx +-1
+    }
 
 }
 
 ////////////////StkMesh2D
 struct StkMesh2D : public Unit
 {
-	Mesh2D * inst;
-	bool  gate;
-	float ampat;
-	float XD;
-	float YD;
-	float Xpos;
-	float Ypos;
-	float decay;
+    Mesh2D * inst;
+    bool  gate;
+    float ampat;
+    float XD;
+    float YD;
+    float Xpos;
+    float Ypos;
+    float decay;
 
 };
 
 extern "C"
 {
-	void StkMesh2D_next(StkMesh2D *unit, int inNumSamples);
-	void StkMesh2D_Ctor(StkMesh2D* unit);
-	void StkMesh2D_Dtor(StkMesh2D* unit);
+    void StkMesh2D_next(StkMesh2D *unit, int inNumSamples);
+    void StkMesh2D_Ctor(StkMesh2D* unit);
+    void StkMesh2D_Dtor(StkMesh2D* unit);
 }
 
 static void StkMesh2D_next_nop(StkMesh2D * unit, int inNumSamples)
 {}
 void StkMesh2D_Ctor(StkMesh2D* unit) {
-	gWorld = unit->mWorld;
-	Stk::setSampleRate( SAMPLERATE );
-	unit->gate = false;
-	unit->ampat	= IN0(2);
-	unit->XD = IN0(3);
-	unit->YD = IN0(4);
-	unit->Xpos = IN0(5);
-	unit->Ypos = IN0(6);
-	unit->decay = IN0(7);
+    gWorld = unit->mWorld;
+    Stk::setSampleRate( SAMPLERATE );
+    unit->gate = false;
+    unit->ampat = IN0(2);
+    unit->XD = IN0(3);
+    unit->YD = IN0(4);
+    unit->Xpos = IN0(5);
+    unit->Ypos = IN0(6);
+    unit->decay = IN0(7);
     
     void* mem = RTAlloc(unit->mWorld,sizeof(Mesh2D));
     if (!mem) {
-			Print("StkMesh2D: RT memory allocation failed on values\n");
-			SETCALC(Unit_next_nop);
-			return;
-		}
-	unit->inst = new(mem) Mesh2D(unit->XD,unit->YD); 
-	if(unit->inst==NULL){
-		Print("StkMesh2D: RT memory allocation failed on instrument\n");
-		SETCALC(StkMesh2D_next_nop);
-		return;
-	}
-	//Print("Mesh created\n");
-	unit->inst->setDecay(unit->decay);
-	SETCALC(StkMesh2D_next);
-	//Print("Mesh created2\n");
+            Print("StkMesh2D: RT memory allocation failed on values\n");
+            SETCALC(Unit_next_nop);
+            return;
+        }
+    unit->inst = new(mem) Mesh2D(unit->XD,unit->YD); 
+    if(unit->inst==NULL){
+        Print("StkMesh2D: RT memory allocation failed on instrument\n");
+        SETCALC(StkMesh2D_next_nop);
+        return;
+    }
+    //Print("Mesh created\n");
+    unit->inst->setDecay(unit->decay);
+    SETCALC(StkMesh2D_next);
+    //Print("Mesh created2\n");
 }
 
 void StkMesh2D_Dtor(StkMesh2D* unit)
 {
 
-	//delete unit->inst;
+    //delete unit->inst;
     RTFree(unit->mWorld, unit->inst);
 }
 
 void StkMesh2D_next(StkMesh2D *unit, int inNumSamples)
 {
-	float *out = OUT(0);
+    float *out = OUT(0);
 
-	float *inp  	= IN(0);
-	bool  gate		= IN0(1) > 0.f;
-	unit->ampat	= IN0(2);
-	unit->XD = IN0(3);
-	unit->YD = IN0(4);
-	unit->Xpos = IN0(5);
-	unit->Ypos = IN0(6);
-	unit->decay = IN0(7);
-	//for(int i=1;i<8;i++)
-	//	Print("IN(%d) is %f",i,IN0(i));
-	//Print("\n");
-	unit->inst->setInputPosition(unit->Xpos,unit->Ypos);
-	if(gate != unit->gate){
-		if(gate){
-			Print("Starting mesh %f\n",unit->ampat);
-		    unit->inst->noteOn(10, unit->ampat);
-		}else{
-			Print("Stopping\n");
-			unit->inst->noteOff(0);
-		}
-		unit->gate = gate;
-	}
+    float *inp = IN(0);
+    bool  gate = IN0(1) > 0.f;
+    unit->ampat = IN0(2);
+    unit->XD = IN0(3);
+    unit->YD = IN0(4);
+    unit->Xpos = IN0(5);
+    unit->Ypos = IN0(6);
+    unit->decay = IN0(7);
+    //for(int i=1;i<8;i++)
+    //  Print("IN(%d) is %f",i,IN0(i));
+    //Print("\n");
+    unit->inst->setInputPosition(unit->Xpos,unit->Ypos);
+    if(gate != unit->gate){
+        if(gate){
+            Print("Starting mesh %f\n",unit->ampat);
+            unit->inst->noteOn(10, unit->ampat);
+        }else{
+            Print("Stopping\n");
+            unit->inst->noteOff(0);
+        }
+        unit->gate = gate;
+    }
 
- 	for (int i=0; i < inNumSamples; ++i)
-	{
-		//out[i] = unit->inst->tick();
-		out[i] = unit->inst->inputTick(inp[i]);
-	}
+    for (int i=0; i < inNumSamples; ++i)
+    {
+        //out[i] = unit->inst->tick();
+        out[i] = unit->inst->inputTick(inp[i]);
+    }
 
 }
 ///////////////////////////////////////
 struct StkGlobals : public Unit
 {
-	char *rawwavePath;
-	bool showWarnings;
-	bool printErrors;
-	int m_valueSize;
+    char *rawwavePath;
+    bool showWarnings;
+    bool printErrors;
+    int m_valueSize;
 
 };
 extern "C"
 {
-	void StkGlobals_next(StkGlobals *unit, int inNumSamples);
-	void StkGlobals_Ctor(StkGlobals* unit);
+    void StkGlobals_next(StkGlobals *unit, int inNumSamples);
+    void StkGlobals_Ctor(StkGlobals* unit);
 }
 
 void StkGlobals_next(StkGlobals *unit, int inNumSamples){
     NodeEnd(&unit->mParent->mNode);
 }
 void StkGlobals_Ctor(StkGlobals* unit) {
-	gWorld = unit->mWorld;
-	Stk::setSampleRate( SAMPLERATE );
-	unit->showWarnings = IN0(0) > 0;
-	unit->printErrors = IN0(1) > 0;
+    gWorld = unit->mWorld;
+    Stk::setSampleRate( SAMPLERATE );
+    unit->showWarnings = IN0(0) > 0;
+    unit->printErrors = IN0(1) > 0;
 
-	const int kVarOffset = 2;
-	unit->m_valueSize = unit->mNumInputs - kVarOffset;
-	
-	if(unit->m_valueSize > 0){
-		const int valuesAllocSize = (unit->m_valueSize +1)* sizeof(char);
-		char * chunk = (char*)RTAlloc(unit->mWorld, valuesAllocSize);
-		if (!chunk) {
-			Print("StkGlobals: RT memory allocation failed on values\n");
-			SETCALC(Unit_next_nop);
-			return;
-		}
-		unit->rawwavePath  = chunk;
-		for(int i=0; i< unit->m_valueSize; i++)
-			unit->rawwavePath[i] = IN0(i + kVarOffset);
-		
-		unit->rawwavePath[unit->m_valueSize] = 0; //null termination
-		Stk::setRawwavePath(unit->rawwavePath);
-	}
-	Stk::showWarnings(unit->showWarnings);
-	Stk::printErrors(unit->printErrors);
-	RTFree(unit->mWorld,unit->rawwavePath);
-	SETCALC(StkGlobals_next);
+    const int kVarOffset = 2;
+    unit->m_valueSize = unit->mNumInputs - kVarOffset;
+    
+    if(unit->m_valueSize > 0){
+        const int valuesAllocSize = (unit->m_valueSize +1)* sizeof(char);
+        char * chunk = (char*)RTAlloc(unit->mWorld, valuesAllocSize);
+        if (!chunk) {
+            Print("StkGlobals: RT memory allocation failed on values\n");
+            SETCALC(Unit_next_nop);
+            return;
+        }
+        unit->rawwavePath  = chunk;
+        for(int i=0; i< unit->m_valueSize; i++)
+            unit->rawwavePath[i] = IN0(i + kVarOffset);
+        
+        unit->rawwavePath[unit->m_valueSize] = 0; //null termination
+        Stk::setRawwavePath(unit->rawwavePath);
+    }
+    Stk::showWarnings(unit->showWarnings);
+    Stk::printErrors(unit->printErrors);
+    RTFree(unit->mWorld,unit->rawwavePath);
+    SETCALC(StkGlobals_next);
 }
 ////////////////////////////////////////
 PluginLoad(StkUnit){
-	ft = inTable;
-	DefineDtorUnit(StkInst);
-	DefineDtorUnit(StkMesh2D);
-	DefineSimpleUnit(StkGlobals);
+    ft = inTable;
+    DefineDtorUnit(StkInst);
+    DefineDtorUnit(StkMesh2D);
+    DefineSimpleUnit(StkGlobals);
 }
