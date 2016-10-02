fb7f4cc0a577b9ec219870390a19bbc43fa2c3cc
Dan Stowell <danstowell@users.sourceforge.net>
2016-10-02 12:27:01 +0100

    update website to point at github

--- website/index.php
+++ website/index.php
@@ -12,19 +12,14 @@ ul.grouplist li { width: 25%; float: left;  clear: none;}
 </style>
 </head>
 <body>
-<h1>UGen plugins for <a href="http://supercollider.sourceforge.net/">SuperCollider</a></h1>
+<h1>UGen plugins for <a href="http://supercollider.github.io/">SuperCollider</a></h1>
 
-<p>SuperCollider &quot;UGen&quot; plugins are extensions for the <a href="http://supercollider.sourceforge.net/">SuperCollider</a> audio synthesis server 
-(see also <a href="http://quarks.sourceforge.net/">quarks</a> to extend the 
-programming language). For community discussion and support see the <a href="http://supercollider.sourceforge.net/mod/resource/view.php?id=41">SuperCollider mailing lists</a>.</p>
+<p>SuperCollider &quot;UGen&quot; plugins are extensions for the <a href="http://supercollider.github.io/">SuperCollider</a> audio synthesis server.</p>
 
-
-<p style="margin: 30px; padding: 30px; border: 1px solid black;">Downloads are available from <a href="https://sourceforge.net/projects/sc3-plugins/files/">https://sourceforge.net/projects/sc3-plugins/files/</a>
+<p style="margin: 30px; padding: 30px; border: 1px solid black;">Downloads are available from <a href="https://github.com/supercollider/sc3-plugins/releases">https://github.com/supercollider/sc3-plugins/releases</a>
 
 <p style="margin: 30px; padding: 30px; border: 1px solid black;"><a href="https://github.com/supercollider/sc3-plugins">Source code is on github</a></p>
 
 
 
-<hr style="clear: both; margin: 10px; border-color: black;" />
-<p style="text-align: right;"><a href="http://sourceforge.net/projects/sc3-plugins/">Sourceforge project page</a> <a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=182627&amp;type=4" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a></p>
 </body></html>
