98150387cb724987a103a6a9c8057c2bb3f318fe
Marcin Pączkowski <dyfeer@gmail.com>
2021-07-29 16:33:10 -0700

    CMake: add search paths for finding FFTW on Windows

--- cmake_modules/FindFFTW3f.cmake
+++ cmake_modules/FindFFTW3f.cmake
@@ -4,14 +4,18 @@ FIND_PATH(
     FFTW3F_INCLUDE_DIR
     NAMES fftw3.h
     HINTS $ENV{FFTW3_DIR}/include
+      "$ENV{ProgramW6432}/fftw"
+      "$ENV{ProgramFiles}/fftw"
     PATHS /usr/local/include
           /usr/include
 )
 
 FIND_LIBRARY(
     FFTW3F_LIBRARY
-    NAMES fftw3f libfftw3f
+    NAMES fftw3f libfftw3f libfftw3f-3.lib
     HINTS $ENV{FFTW3_DIR}/lib
+      "$ENV{ProgramW6432}/fftw"
+      "$ENV{ProgramFiles}/fftw"
     PATHS /usr/local/lib
           /usr/lib
 )
